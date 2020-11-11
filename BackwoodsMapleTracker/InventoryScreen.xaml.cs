2109ccaa63fd8c04906cb68dbd6d1a3de3a00739
Isaiah Ley <isaiahley20@gmail.com>
2020-11-10 22:04:59 -0600

    InventoryScreen

---
+++ BackwoodsMapleTracker/InventoryScreen.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BackwoodsMapleTracker
+{
+    /// <summary>
+    /// Interaction logic for InventoryScreen.xaml
+    /// </summary>
+    public partial class InventoryScreen : Window
+    {
+        public InventoryScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Click_Pints_Edit(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn_Click_Quarts_Edit(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn_Click_Half_Pints_Edit(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn_Click_Pints_Plus_12(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn_Click_Pints_Plus_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn_Click_Pints_Minus_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn_Click_Quarts_Plus_12(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn_Click_Quarts_Plus_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn_Click_Quarts_Minus_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn_Click_Half_Pints_Plus_12(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn_Click_Half_Pints_Plus_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn_Click_Half_Pints_Minus_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
