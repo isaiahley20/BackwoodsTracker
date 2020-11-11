b4e2a01d74147f1aba31b096a56a816bb689c798
Isaiah Ley <isaiahley20@gmail.com>
2020-11-10 20:58:12 -0600

    DailyCostRecord object

---
+++ BackwoodsMapleTracker/DailyCostRecord.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackwoodsMapleTracker
+{
+    class DailyCostRecord
+    {
+        public string date { get; }
+        public string name { get; }
+        public double cost { get; }
+
+        public DailyCostRecord(string dateValue, string nameValue, double costValue)
+        {
+            date = dateValue;
+            name = nameValue;
+            cost = costValue;
+        }
+    }
+}
