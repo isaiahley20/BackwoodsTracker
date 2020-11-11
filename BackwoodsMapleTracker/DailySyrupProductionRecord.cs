f13db5acfff98203f5f1a4abaa02037b75978c81
Isaiah Ley <isaiahley20@gmail.com>
2020-11-10 20:55:13 -0600

    DailySyrupProductionRecord object

---
+++ BackwoodsMapleTracker/DailySyrupProductionRecord.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackwoodsMapleTracker
+{
+    class DailySyrupProductionRecord
+    {
+        public string date { get; }
+        public double syrupMadePints { get; }
+        public double sapUsedPints { get; }
+
+        public DailySyrupProductionRecord(string dateValue, double syrupMadePintsValue, double sapUsedPintsValue)
+        {
+            date = dateValue;
+            syrupMadePints = syrupMadePintsValue;
+            sapUsedPints = sapUsedPintsValue;
+        }
+        public string getSyrupSapRatio()
+        {
+            return "1 : " + Math.Round((syrupMadePints / sapUsedPints), 1);
+        }
+
+    }
+}
