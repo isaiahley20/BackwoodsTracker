3185cbb035ca0b32947b714bd0834f26d5185503
Isaiah Ley <isaiahley20@gmail.com>
2020-11-15 14:25:08 -0600

    Control class

---
+++ BackwoodsMapleTracker/Control.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackwoodsMapleTracker
+{
+    class Control
+    {
+        private string mostRecentSapOverSyrupRatio()
+        {
+            return "";
+        }
+        private string calcExpectedNextSapOverSyrupRatio()
+        {
+            return "";
+        }
+        private int getPintBottlesAvail()
+        {
+            return 0;
+        }
+        private int getHalfPintBottlesAvail()
+        {
+            return 0;
+        }
+        private int getQuartBottlesAvail()
+        {
+            return 0;
+        }
+        private List<DailySyrupProductionRecord> getDailySyrupProducedList()
+        {
+            List<DailySyrupProductionRecord> temp = new List<DailySyrupProductionRecord>();
+            return temp;
+        }
+    
+    }
+}
